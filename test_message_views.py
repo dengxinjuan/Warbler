b9c595cad89055c6d449263f22ab650cb579d651
xinjuan Deng <dengxinj@gmail.com>
2020-11-11 13:57:40 -0500

    working on test message view

--- test_message_views.py
+++ test_message_views.py
@@ -91,37 +91,12 @@ class MessageViewTestCase(TestCase):
             resp = c.post("/messages/new", data={"text": "Hello"}, follow_redirects=True)
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Access unauthorized", str(resp.data))
-    
-    def test_message_show(self):
 
-        msg = Message( id=2222, text="first text message", user_id = self.testuser_id)
-        db.session.add(msg)
-        db.session.commit()
-
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
-                m = Message.query.get(2222)
-                resp = c.get(f'/messages/{m.id}')
-
-                self.assertEqual(resp.status_code,200)
-                self.assertIn(m.text,str(resp.data))
-    
-    def test_invalid_message_show(self):
-        """"test whetehr invalid message show"""
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
-
-                resp = c.get("/messages/9878899999")
-                self.assertEqual(resp.status_code,404)
-    
-    
     def test_message_delete(self):
 
         m = Message(
             id=2222,
-            text="something to delete",
+            text="a test message",
             user_id=self.testuser_id
         )
         db.session.add(m)
@@ -136,28 +111,47 @@ class MessageViewTestCase(TestCase):
 
             m = Message.query.get(2222)
             self.assertIsNone(m)
+
     
     def test_unauthorized_message_delete(self):
 
-        user2 = User.signup(username="baduser", email="baduser@gmail.com", password="123456",image_url="None")
-        user2.id = 22222
-        m = Message( id=1234, text="somethingtext",user_id=self.testuser_id)
+        # A second user that will try to delete the message
+        u = User.signup(username="unauthorized-user",
+                        email="testtest@test.com",
+                        password="password",
+                        image_url=None)
+        u.id = 76543
 
-        db.session.add_all([user2,m])
+        #Message is owned by testuser
+        m = Message(
+            id=1234,
+            text="a test message",
+            user_id=self.testuser_id
+        )
+        db.session.add_all([u, m])
         db.session.commit()
 
         with self.client as c:
             with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = 22222
-                resp = c.post("/messages/22222/delete",follow_redirects=True)
-                self.assertEqual(resp.status_code,200)
-                self.assertIn("Access unauthorized",str(resp.data))
+                sess[CURR_USER_KEY] = 76543
 
-                m = Message.query.get(22222)
-                self.assertIsNotNone(m)
+            resp = c.post("/messages/1234/delete", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Access unauthorized", str(resp.data))
+
+            m = Message.query.get(1234)
+            self.assertIsNotNone(m)
+
 
     
 
+    #test invalid message code should be 404??????? I dont know why last test fail???
+    def test_invalid_message_show(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+                resp = c.get('/messages/9878899999')
+                self.assertEqual(resp.status_code, 404)
 
 
 
